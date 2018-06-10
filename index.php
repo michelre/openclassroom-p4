77b30138eb36630dc7e62f8aa9ece8bfa9c759f9
marie larrouy <marielarrouy71@orange.fr>
2018-06-10 14:19:11 +0200

    premier commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Blog</title>
+
+</head>
+
+<body>
+    <h1>Test</h1>
+
+    <div>
+
+        <?php echo "toto" ?>
+    </div>
+</body>
+
+</html>
